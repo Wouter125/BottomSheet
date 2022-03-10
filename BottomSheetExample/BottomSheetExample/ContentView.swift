eb41740e1dc40f6264a255ea673b9c2c75594eea
Wouter <wouter@thisisnota.studio>
2022-03-10 23:52:01 +0100

    fix(workspace): remove workspace and move package up

--- BottomSheetExample/BottomSheetExample/ContentView.swift
+++ BottomSheetExample/BottomSheetExample/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import BottomSheet
+//import BottomSheet
 
 enum BottomSheetPosition: CGFloat, CaseIterable {
     case bottom = 182
@@ -19,84 +19,84 @@ struct ContentView: View {
 
     var body: some View {
         ZStack(alignment: .bottom) {
-            BottomSheetView(
-                position: $position,
-                header: {
-                    VStack(spacing: 0) {
-                        Button(action: {
-                            if position != .top {
-                                position = .top
-                            } else {
-                                position = .middle
-                            }
-                        }, label: {
-                            Rectangle()
-                                .frame(
-                                    width: 36,
-                                    height: 5,
-                                    alignment: .center
-                                )
-                                .foregroundColor(Color(UIColor.systemGray3))
-                                .cornerRadius(2.5)
-                        })
-
-                        HStack {
-                            VStack(alignment: .leading, spacing: 2) {
-                                Text("Business News")
-                                    .font(.title)
-                                    .fontWeight(.heavy)
-                                Text("From Yahoo Finance")
-                                    .foregroundColor(Color(UIColor.secondaryLabel))
-                            }
-                            .padding(.top, 10)
-                            .padding(.bottom, 16)
-
-                            Spacer()
-                        }
-
-                        Divider()
-                         .frame(height: 1)
-                         .background(Color(UIColor.systemGray6))
-                    }
-                    .padding(.top, 8)
-                    .padding(.horizontal, 16)
-                },
-                content: {
-                    VStack(spacing: 0) {
-                        ScrollView {
-                            ForEach(0..<12, id: \.self) { _ in
-                                newsRow
-                            }
-                        }
-
-                        Spacer(minLength: 54)
-                    }
-                    // Enable or disable the following line if we want to run the scrollview outside the safe-area
-                    // .edgesIgnoringSafeArea([.bottom])
-                }
-            )
-            .animationCurve(mass: 1, stiffness: 250)
-            .onBottomSheetDrag { translation in
-                print("Translation", translation)
-            }
-
-            VStack(spacing: 0) {
-                Divider()
-                 .frame(height: 1)
-                 .background(Color(UIColor.systemGray6))
-
-                HStack {
-                    Text("Yahoo Finance")
-                    Spacer()
-                }
-                .padding(.horizontal, 16)
-                .padding(.vertical, 16)
-            }
-            .background(
-                Color(UIColor.systemBackground)
-                    .edgesIgnoringSafeArea([.bottom])
-            )
-            .zIndex(1)
+//            BottomSheetView(
+//                position: $position,
+//                header: {
+//                    VStack(spacing: 0) {
+//                        Button(action: {
+//                            if position != .top {
+//                                position = .top
+//                            } else {
+//                                position = .middle
+//                            }
+//                        }, label: {
+//                            Rectangle()
+//                                .frame(
+//                                    width: 36,
+//                                    height: 5,
+//                                    alignment: .center
+//                                )
+//                                .foregroundColor(Color(UIColor.systemGray3))
+//                                .cornerRadius(2.5)
+//                        })
+//
+//                        HStack {
+//                            VStack(alignment: .leading, spacing: 2) {
+//                                Text("Business News")
+//                                    .font(.title)
+//                                    .fontWeight(.heavy)
+//                                Text("From Yahoo Finance")
+//                                    .foregroundColor(Color(UIColor.secondaryLabel))
+//                            }
+//                            .padding(.top, 10)
+//                            .padding(.bottom, 16)
+//
+//                            Spacer()
+//                        }
+//
+//                        Divider()
+//                         .frame(height: 1)
+//                         .background(Color(UIColor.systemGray6))
+//                    }
+//                    .padding(.top, 8)
+//                    .padding(.horizontal, 16)
+//                },
+//                content: {
+//                    VStack(spacing: 0) {
+//                        ScrollView {
+//                            ForEach(0..<12, id: \.self) { _ in
+//                                newsRow
+//                            }
+//                        }
+//
+//                        Spacer(minLength: 54)
+//                    }
+//                    // Enable or disable the following line if we want to run the scrollview outside the safe-area
+//                    // .edgesIgnoringSafeArea([.bottom])
+//                }
+//            )
+//            .animationCurve(mass: 1, stiffness: 250)
+//            .onBottomSheetDrag { translation in
+//                print("Translation", translation)
+//            }
+//
+//            VStack(spacing: 0) {
+//                Divider()
+//                 .frame(height: 1)
+//                 .background(Color(UIColor.systemGray6))
+//
+//                HStack {
+//                    Text("Yahoo Finance")
+//                    Spacer()
+//                }
+//                .padding(.horizontal, 16)
+//                .padding(.vertical, 16)
+//            }
+//            .background(
+//                Color(UIColor.systemBackground)
+//                    .edgesIgnoringSafeArea([.bottom])
+//            )
+//            .zIndex(1)
         }
         .background(
             Color(UIColor.systemFill)
