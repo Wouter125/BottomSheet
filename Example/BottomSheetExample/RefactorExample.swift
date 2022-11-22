186a0395ea960974a5b37674752aa25b9b16402c
Wouter <wouter@thisisnota.studio>
2022-11-22 23:06:11 +0100

    feat: included dragging on scroll section

--- Example/BottomSheetExample/RefactorExample.swift
+++ Example/BottomSheetExample/RefactorExample.swift
@@ -44,7 +44,7 @@ struct RefactorExample: View {
                         }
                     }
                     .presentationDetentsPlus(
-                        [.medium, .large],
+                        [.small, .medium, .large],
                         selection: $selectedDetent
                     )
                 }
